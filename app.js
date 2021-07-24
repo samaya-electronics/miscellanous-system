988065171867201f3606a029055c4b4a7c4fd98b
karimkohel <kareemkohel@gmail.com>
2021-07-24 16:11:13 +0200

    added some test data

--- app.js
+++ app.js
@@ -7,31 +7,45 @@ app.use(express.json())
 
 users = [
     {
+        id:1,
         username:"karim",
         password:"test123"
     },
     {
+        id:2,
         username:"osama",
         password:"test123"
-    }
+    },
+    {
+        id:3,
+        username:"karim2",
+        password:"test123"
+    },
+    {
+        id:4,
+        username:"osama2",
+        password:"test123"
+    },
+    {
+        id:5,
+        username:"osama3",
+        password:"test123"
+    },    
+
 ]
 
 app.get('/users', (req, res)=>{
     res.json(users)
 })
 
-app.post('/users', async (req, res)=>{
-    try {
-        // const userSalt = await bcrypt.genSalt()
-        const hashedP = await bcrypt.hash(req.body.password, 10)
-        const user = {username: req.body.username, password: hashedP}
-        users.push(user)
-        console.log(user)
-        res.status(201).send()
-    }
-    catch {
-        res.status(500).send()
-    }
+app.get('/users/:id', (req, res)=>{
+    const {id} = req.params
+    res.json(users.find(user => user.id === Number(id)))
 })
+
+
+// app.post('/users', async (req, res)=>{
+
+// })
 
 app.listen(5000)
