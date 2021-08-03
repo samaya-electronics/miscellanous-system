d89bed13d4cfe3536e4b5a9a1a9934ba3f01ad50
Mohameosama <moandosama1163@gmail.com>
2021-08-03 23:46:59 +0200

    api fopr catagories

---
+++ src/routes/catagories.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+router.use(express.json())
+const catagories = require('./cat')
+
+
+router.get('/',(req,res)=>{
+    res.json(catagories)
+})
+
+
+router.get('/:id',(req,res)=>{
+    const found = catagories.some(catagories=>catagories.id===parseInt(req.params.id))
+    if(found){
+    res.json(catagories.filter(catagories=> catagories.id === req.params.id))
+    } else{
+        res.statusCode(400).json({msg:'catagorie not found'})
+    }
+})
+
+
+module.exports = router
+
