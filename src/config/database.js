f625958ce350d17a9615e7493b3edbded5bd67dd
karim kohel <kareemkohel@gmail.com>
2021-08-05 09:45:17 +0200

    solved some sequalize db connection problem and set up dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const env = require('dotenv').config();
 
 // inits
-const sqliz = new Sequelize('test_db', 'root', process.env.PASSWORD, {
+const sqliz = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
 }
