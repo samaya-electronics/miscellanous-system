90c6555a0771713476dd9aeefe9da676e06d3927
karim kohel <kareemkohel@gmail.com>
2021-08-05 08:42:55 +0200

    redundunt merge

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,6 @@
 const express = require('express')
-<<<<<<< HEAD
 //const { SequelizeScopeError } = require('sequelize/types')
-=======
-// const { SequelizeScopeError } = require('sequelize/types')
->>>>>>> f45cc187fb7d72076c3d6d8fae7e9d1d8c23604d
+
 const router = express.Router()
 router.use(express.json())
 
