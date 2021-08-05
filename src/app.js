d550769aa6c70a4984385a6640aa1cde442b5fe1
Mohameosama <moandosama1163@gmail.com>
2021-08-05 10:41:50 +0200

    adding cors to api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const auth = require('./routes/auth');
 const catagories = require('./routes/catagories');
-
+const cors = require('cors')
 //init
 const app = express()
 
@@ -9,7 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 5000 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-
+app.use(express.cors)
 
 // router
 app.use('/auth', auth)
