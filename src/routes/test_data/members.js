2db52d8cc99648909a4790c933671eac1400bb81
Mohameosama <moandosama1163@gmail.com>
2021-08-10 10:11:56 +0200

    adding delete edit create member

---
+++ src/routes/test_data/members.js
@@ -0,0 +1,24 @@
+const members= [
+    {
+        id:1,
+        name:'osama',
+        status:'pending',
+        email:'osama@gmail.com'
+    },
+    {
+        id:2,
+        name:'kariem',
+        status:'pending',
+        email:'karim@gmail.com'
+    },
+    {
+        id:3,
+        name:'hazem',
+        status:'active',
+        email:'hazem@gmail.com'
+    }
+
+]
+
+
+module.exports = members
