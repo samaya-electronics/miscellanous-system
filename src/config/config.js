b1ac59f5c6ab1ef137e28503fbec3fe00ff79bf3
karim kohel <kareemkohel@gmail.com>
2021-08-11 09:00:14 +0200

    sequelize config and init

---
+++ src/config/config.js
@@ -0,0 +1,25 @@
+const env = require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
