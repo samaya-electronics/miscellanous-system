5c201b347646032cedc45b911d27418752effbcc
karimkohel <kareemkohel@gmail.com>
2021-08-13 02:12:32 +0200

    changed username to name to avoid default values error

--- src/config/config.js
+++ src/config/config.js
@@ -1,22 +1,22 @@
-const env = require('dotenv').config();
+require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.USERNAME,
+    "username": process.env.NAME,
     "password": process.env.PASSWORD,
     "database": process.env.DATABASE,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
-    "username": process.env.USERNAME,
+    "username": process.env.NAME,
     "password": process.env.PASSWORD,
     "database": process.env.DATABASE,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.USERNAME,
+    "username": process.env.NAME,
     "password": process.env.PASSWORD,
     "database": process.env.DATABASE,
     "host": "127.0.0.1",
