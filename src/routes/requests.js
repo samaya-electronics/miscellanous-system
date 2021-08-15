7d9f4702e15f4a6d765b961e025d5072d6111fda
Mohameosama <moandosama1163@gmail.com>
2021-08-15 15:57:00 +0200

    adding request controller

---
+++ src/routes/requests.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const request = require('../controllers/requestController')
+
+
+router.get('/:id',request.getrequestById)
+
+router.get('/',request.getAllrequests)
+
+router.post('/',request.createrequest)
+
+router.delete('/:id',request.deleterequest)
+
+
+module.exports = router
