98e6210067f549cc46d1aebe59acd5e2837f0d00
karimkohel <kareemkohel@gmail.com>
2021-08-16 00:37:16 +0200

    prepped the project for heavy duty testing AGILE SCRUM

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
-const app = require('./config/server');
-const { sequelize } = require('./models')
+const express = require('express')
+const cors = require('cors')
 
-const PORT = process.env.PORT || 5000 
+const homeRouter = require('./routes/homeRouter')
+const authRouter = require('./routes/authRouter')
+const categoriesRouter = require('./routes/categoriesRouter')
+const itemsRouter = require('./routes/itemsRouter')
+const usersRouter = require('./routes/usersRouter')
+const requestRouter = require('./routes/requestsRouter')
+const permissionRouter = require('./routes/permissionRouter')
+const rolesRouter = require('./routes/rolesRouter')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
 
-app.listen(PORT, async () => {
-    console.log(`Server started on port ${PORT}`)
-    try{
-        await sequelize.sync({force: true})
-        console.log("Database synced")
-    }
-    catch(err){
-        console.log(err)
-    }
-})
+
+// router
+app.use('/', homeRouter)
+app.use('/auth', authRouter)
+app.use('/categories', categoriesRouter)
+app.use('/items', itemsRouter)
+app.use('/users',usersRouter)
+app.use('./routers',requestRouter)
+app.use('./permissions',permissionRouter)
+app.use('/roles',rolesRouter)
+
+module.exports = app
---
+++ src/server.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+const { sequelize } = require('./models')
+
+const PORT = process.env.PORT || 5000 
+
+app.listen(PORT, async () => {
+    console.log(`Server started on port ${PORT}`)
+    try{
+        await sequelize.sync({force: true})
+        console.log("Database synced")
+    }
+    catch(err){
+        console.log(err)
+    }
+})
