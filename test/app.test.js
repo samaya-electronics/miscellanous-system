17e99de56b71779481d7b43381845425ed9d301d
karim kohel <kareemkohel@gmail.com>
2021-08-16 08:30:01 +0200

    main entry point test trial

--- test/app.test.js
+++ test/app.test.js
@@ -1,2 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
+
+describe('APP test', () => {
+  it('GET / --> App is working, returns true', () => {
+    return request(app).get('/')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            homepage: true
+          })
+        )
+      })
+  })
+})
