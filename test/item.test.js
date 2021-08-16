b6b43e5793a5382a5e9f782048b92c9c69b33311
karim kohel <kareemkohel@gmail.com>
2021-08-16 16:49:40 +0200

    more items test

--- test/item.test.js
+++ test/item.test.js
@@ -30,15 +30,19 @@ describe('Item I/O --> Category dependent', () => {
       expect(res.body.name).toEqual(expected_name)
   })
 
-  test('POST /items --> Create 1 item, Category dependent', async () => {
+  test.each([
+    ["test-item-1", "test-loc-1", 58, 30, 1],
+    ["test-item-2", "test-loc-2", 52, 25, 2],
+    ["test-item-3", "test-loc-3", 55, 20, 2],
+  ])('POST /items --> Create 3 item, Category dependent', async (test_name, test_loc, test_q, test_thresh, test_cat_id) => {
     const item_res = await request(app)
       .post('/items')
       .send({
-        name: "Item 1 test",
-        quantity: 50,
-        threshold: 20,
-        location: "ta7t el selem",
-        category_id: 2
+        name: test_name,
+        quantity: test_q,
+        threshold: test_thresh,
+        location: test_loc,
+        category_id: test_cat_id
       })
       expect(item_res.statusCode).toEqual(200)
   })
@@ -48,6 +52,7 @@ describe('Item I/O --> Category dependent', () => {
       .get('/items')
 
     expect(res.statusCode).toEqual(200)
+    expect(res.body.length).toEqual(3)
   })
 
   test('GET /items/:pk --> get item by pk to check it is created in DB', async () => {
@@ -55,11 +60,11 @@ describe('Item I/O --> Category dependent', () => {
       .get('/items/1')
 
     expect(res.statusCode).toEqual(200)
-    expect(res.body.name).toEqual('Item 1 test')
-    expect(res.body.location).toEqual("ta7t el selem")
-    expect(res.body.threshold).toEqual(20)
-    expect(res.body.quantity).toEqual(50)
-    expect(res.body.category_id).toEqual(2)
+    expect(res.body.name).toEqual("test-item-1")
+    expect(res.body.location).toEqual("test-loc-1")
+    expect(res.body.threshold).toEqual(30)
+    expect(res.body.quantity).toEqual(58)
+    expect(res.body.category_id).toEqual(1)
   })
 
 })
