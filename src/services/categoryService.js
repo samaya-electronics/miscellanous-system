019530c4ce5a2614671b78bed6de381418ce647c
karimkohel <kareemkohel@gmail.com>
2021-08-18 20:27:43 +0200

    reafactored category service

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,24 +1,30 @@
 const { Category } = require('../models')
 
 const createCategory = async (data) => {
-
+    const result = {}
     try{
         const category = await Category.create({
             name: data.name
         })
-        return {
-            category: category,
-            msg: "Category Created"
-        }
+        result.category = category,
+        result.msg = "Category Created"
     }
     catch(err){
-        return {
-            err: err,
-            msg: "Could not create Category, try again later."
-        }
+        result.err = err
+        result.msg = "Could not create Category, try again later."
     }
-    
+    return result
 }
+
+
+// const getCategories = async () => {
+//     try{
+//         const categories = await Category.findAll()
+//     }
+//     catch(err){
+//         return
+//     }
+// }
 
 module.exports = {
     createCategory,
