b3b4fd784122ef229576101aa797dc9214058a38
karimkohel <kareemkohel@gmail.com>
2021-08-19 16:10:03 +0200

    changed back pk to id

--- src/routes/requestsRouter.js
+++ src/routes/requestsRouter.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const request = require('../controllers/requestController')
 
-router.get('/:pk',request.getrequestById)
+router.get('/:id',request.getrequestById)
 
 router.get('/',request.getAllrequests)
 
 router.post('/',request.createrequest)
 
-router.delete('/:pk',request.deleterequest)
+router.delete('/:id',request.deleterequest)
 
 
 module.exports = router
