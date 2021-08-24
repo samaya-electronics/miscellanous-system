70da9e8330f265b26acf35632c3dc3fb97d4b6f8
karim kohel <kareemkohel@gmail.com>
2021-08-24 08:40:52 +0200

    finished testing

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 const app = require('./app');
 const { sequelize } = require('./database/models')
 
-const PORT = process.env.PORT || 5000 
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, async () => {
-    console.log(`Server started on port ${PORT}`)
-    try{
-        await sequelize.sync({force: true})
-        console.log("Database synced")
-    }
-    catch(err){
-        console.log(err)
-    }
+	console.log(`Server started on port ${PORT}`)
+	try{
+		await sequelize.sync({force: true})
+		console.log("\nDatabase synced")
+	}
+	catch(err){
+		console.log(err)
+	}
 })
