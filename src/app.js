80311ba810eaa0779977563339e54c0c521f68f8
karim kohel <kareemkohel@gmail.com>
2021-08-29 11:31:49 +0200

    some organization

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const app = express()
 
 const homeRouter = require('./routes/homeRouter')
 const authRouter = require('./routes/authRouter')
@@ -10,18 +11,16 @@ const requestRouter = require('./routes/requestsRouter')
 const permissionRouter = require('./routes/permissionRouter')
 const rolesRouter = require('./routes/rolesRouter')
 const authMiddleware = require('./middleware/authMiddleware')
-const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
-
 
 // router
 app.use('/', homeRouter)
 app.use('/auth', authRouter)
 
 app.use(authMiddleware.authenticateToken)
-
 app.use('/categories', categoriesRouter)
 app.use('/items', itemsRouter)
 app.use('/users', usersRouter)
