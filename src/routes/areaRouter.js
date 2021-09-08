6cad22867e72e2d13726e37cd1e144ed09d84fc2
Mohameosama <moandosama1163@gmail.com>
2021-09-08 10:06:20 +0200

    adding controllers and routes of locations

--- src/routes/areaRouter.js
+++ src/routes/areaRouter.js
@@ -4,8 +4,9 @@ const area = require('../controllers/areaController')
 const { onlyPassRoles } = require('../middleware/authMiddleware')
 
 
-router.get('/:id',onlyPassRoles('superuser'),area.getAreaById)
 router.get('/',onlyPassRoles('superuser'),area.getAreas)
+router.get('/:id',onlyPassRoles('superuser'),area.getAreaById)
+router.get('/:id/sections',onlyPassRoles('superuser'),area.getSectionsByArea)
 router.post('/',onlyPassRoles('superuser'),area.postArea)
 router.put('/:id',onlyPassRoles('superuser'),area.updateArea)
 router.delete('/:id',onlyPassRoles('superuser'),area.deleteArea)
--- src/routes/sectionRouter.js
+++ src/routes/sectionRouter.js
@@ -4,8 +4,9 @@ const section = require('../controllers/sectionController')
 const { onlyPassRoles } = require('../middleware/authMiddleware')
 
 
-router.get('/:id',onlyPassRoles('superuser'),section.getSectionById)
 router.get('/',onlyPassRoles('superuser'),section.getSections)
+router.get('/:id',onlyPassRoles('superuser'),section.getSectionById)
+router.get('/:id/boxes',onlyPassRoles('superuser'),section.getBoxesBySection)
 router.post('/',onlyPassRoles('superuser'),section.postSection)
 router.put('/:id',onlyPassRoles('superuser'),section.updateSection)
 router.delete('/:id',onlyPassRoles('superuser'),section.deleteSection)
